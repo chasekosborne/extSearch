6add7af339c3d14bad6d4ee9e2a528616d470983
Alex <fafabikualex@gmail.com>
2026-02-01 17:27:26 -0500

    code using flask, and changes dynamically

--- main.py
+++ main.py
@@ -2,9 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/base.html')
+@app.route('/')
 def home():
-    return render_template('base.html')
+    return render_template('home.html')
 
+@app.route('/what-is-fit')
+def what_is_fit():
+    return render_template('what-is-fit.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/fit')
+def fit():
+    return render_template('fit.html')
+    
 if __name__ == '__main__':
     app.run(debug=True)
