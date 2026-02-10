384d1dce97d5c583fccd6ae695d23fe14eb96513
alexT <alexs.tregub@gmail.com>
2026-02-10 12:53:08 -0500

    Initial template.

---
+++ static/scripts/collisions.js
@@ -0,0 +1,20 @@
+///// Alex Tregub
+///// 2026-02-10
+///// collisions.js
+///// ===========
+/* Provides 'standard' interface for collision checks between squares.
+    To be updated later, converted to OO approach...? Keep legacy function
+    names as possible.
+    - USING STRICT SYNTAX CHECKING, KEEP IF POSSIBLE.
+    - (Maj.Min.Rev)
+    
+    - Working with arrays of corners [A,B,C,D] where A-B A-D adjacent. A is [x,y] array. CONVERSIONS NEEDED
+
+    VERSION 1.0.0 
+*/
+///// ===========
+"use strict";
+
+function checkCollides(sq1,sq2) { // Simple vector collision check
+    // ...
+}
